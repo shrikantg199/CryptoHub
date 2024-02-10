a4b54a64068b3c8f7cd0a9c789158117ba0ce5da
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-10 13:45:12 +0530

    initial commit

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { BaseUrl } from "./BaseUrl";
+import Loader from "./Loader";
+import Header from "./Header";
+import CoinCard from "./CoinCard";
 
 function Coin() {
-  return <div>fdsdfgfdg</div>;
+  const [loading, setLoading] = useState(true);
+  const [currency, setCurrency] = useState("usd");
+  const currencySymbol = currency === "inr" ? "₹" : "$";
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    const fetchExchangesData = async () => {
+      try {
+        const response = await axios.get(
+          `${BaseUrl}/coins/markets?vs_currency=${currency}`
+        );
+        console.log(response.data);
+        setCoins(response.data);
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching exchanges:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchExchangesData();
+  }, [currency]);
+  return (
+    <div>
+      {" "}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="">
+          <Header />
+        </div>
+      )}
+      <>
+        <div className="  bg-[#10b97e]/50 fixed h-full w-screen border-1 overflow-y-auto   ">
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="fixed right-60 ">
+              {" "}
+              <button
+                onClick={() => setCurrency("inr")}
+                className="lg:bg-white/50 bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
+              >
+                INR
+              </button>
+              <button
+                className="lg:bg-white/50 bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
+                onClick={() => setCurrency("usd")}
+              >
+                USD
+              </button>
+            </div>
+          )}
+
+          {coins.map((item, index) => (
+            <CoinCard
+              id={item.id}
+              image={item.image}
+              currencySymbol={currencySymbol}
+              name={item.name.slice(0, 11)}
+              price={item.current_price.toFixed(0)}
+              cap={item.market_cap_change_percentage_24h.toFixed(2)}
+            />
+          ))}
+        </div>
+      </>
+    </div>
+  );
 }
 
 export default Coin;
