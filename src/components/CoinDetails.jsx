65d74b385b548d6b867af43540684ee279c2661b
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-12 10:08:54 +0530

    initial commit

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -27,14 +27,26 @@ function CoinDetails() {
 
     getCoin();
   }, [id]);
+
+  const formatDate = () => {
+    const date = coin.last_updated;
+    const dateObject = new Date(date);
+    const year = dateObject.getFullYear();
+    const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
+    const day = dateObject.getDate().toString().padStart(2, "0");
+    const hours = dateObject.getHours().toString().padStart(2, "0");
+    const minutes = dateObject.getMinutes().toString().padStart(2, "0");
+    const seconds = dateObject.getSeconds().toString().padStart(2, "0");
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
   return (
     <>
       {loading ? (
         <Loader />
       ) : (
-        <div className="bg-[#10b97e]/70 h-screen w-screen fixed block lg:flex overflow-y-auto lg:overflow-hidden ">
-          <div className="bg-white full lg:w-[30%] h-screen   lg:overflow-y-auto   ">
-            <div className=" flex justify-evenly  bg-green-500/40 items-center shadow-sm  w-full rounded-sm h-14">
+        <div className="bg-[#181a20] h-screen w-screen fixed block lg:flex overflow-y-auto lg:overflow-hidden ">
+          <div className="bg-[#181a20] text-white full lg:w-[30%] h-screen   lg:overflow-y-auto  border-[1px] border-[#2B3139] ">
+            <div className=" flex justify-evenly  bg-[#181a20]items-center shadow-sm  w-full rounded-sm h-14">
               <span className="text-3xl font-semibold ">{coin.name}</span>
               <div className="flex justify-center gap-4">
                 <IoMdStarOutline className="text-3xl" />
@@ -51,7 +63,7 @@ function CoinDetails() {
               </div>
 
               <div className="text-5xl flex justify-center">
-                <h2 className="m-6 font-bold">
+                <h2 className="m-6 font-bold hover:scale-110 transition-all">
                   ₹ {coin.market_data?.current_price["inr"]}
                 </h2>
               </div>
@@ -73,12 +85,12 @@ function CoinDetails() {
                 </div>
               </div>
 
-              <h1 className="text-lg m-4 text-gray-600">
-                <span className="font-bold">Last Update-</span>
-                {coin.last_updated}
+              <h1 className="text-lg m-4 ">
+                <span className="font-bold">Last Update- </span>
+                {formatDate()}
               </h1>
               <div className="font-bold text-3xl lg:m-3 ml-12 flex  ">
-                <IoMdPulse className="text-orange-500" /> #
+                <IoMdPulse className="text-orange-500 ml-10" /> #
                 {coin.market_data?.market_cap_rank}
               </div>
               <div className="bg-gray-500/15 rounded-xl  h-auto lg:w-[350px] p-2 lg:ml-8 m-2">
@@ -87,7 +99,7 @@ function CoinDetails() {
                     {" "}
                     <span className="font-bold text-lg"> Description: </span>
                     <span className="">
-                      {coin?.description["en"]?.split(".")[0]}.
+                      {coin?.description?.en?.split(".")[0]}.
                     </span>
                   </div>
                 </div>
@@ -104,7 +116,7 @@ function CoinDetails() {
             </div>
           </div>
 
-          <div className="bg-white w-full lg:w-[70%] h-full border-1   ">
+          <div className="bg-[#181a20] w-full lg:w-[70%] h-full text-white  ">
             <CoinChartData
               img={coin.image?.large}
               price={coin.market_data?.current_price["inr"]}
