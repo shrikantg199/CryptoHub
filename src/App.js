8bc5d35c485da189e636eb22ce2d32de3a057d25
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-12 10:10:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Exchanges from "./components/Exchanges";
 import Coin from "./components/Coin";
 import CoinDetails from "./components/CoinDetails";
+import NewsCrypto from "./components/NewsCrypto";
+import Trendings from "./components/Trendings";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Exchanges />} />
-        <Route path="/coin" element={<Coin />} />
+        <Route path="/coins" element={<Coin />} />
         <Route path="/coins/:id" element={<CoinDetails />} />
+        <Route path="/news" element={<NewsCrypto />} />
+        <Route path="/trending" element={<Trendings />} />
       </Routes>
     </>
   );
