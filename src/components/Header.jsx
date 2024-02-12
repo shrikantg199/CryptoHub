50adec154b174d5642d735aedc85b1b9947eeb51
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-12 10:09:12 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -8,44 +8,49 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
-  Input,
 } from "@nextui-org/react";
 import img from "../assets/logo.png";
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = ["Home", "coins"];
+  const menuItems = ["Home", "coins", "Trending"];
 
   return (
     <Navbar
       onMenuOpenChange={setIsMenuOpen}
-      className="flex justify-between gap-4 items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  h-16  "
+      className="flex justify-between gap-4 items-center bg-[#181a20]  h-16  "
     >
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
+          className="sm:hidden text-white"
         />
         <Link href="/">
           <NavbarBrand>
             <p className="font-bold text-inherit text-2xl text-white font-serif">
               <div className="flex justify-center items-center">
                 {" "}
-                <img src={img} alt="" className="h-18 w-16 m-2" />
-                CryptoHub
+                <img
+                  src={img}
+                  alt=""
+                  className="lg:h-18 lg:w-16 m-1 h-10 w-10"
+                />
+                <span className="text-base lg:text-3xl"> CryptoHub</span>
               </div>
             </p>
           </NavbarBrand>
         </Link>
       </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-12 " justify="center">
-        <Input
-          type="Coin Search"
-          label="Coin Search"
-          className="w-[500px] px-10 m-4 "
-        />
+      <NavbarContent
+        className="hidden sm:flex gap-12  font-semibold "
+        justify="center"
+      >
         <NavbarItem>
-          <Link color="foreground" href="/" className="text-2xl  text-white ">
+          <Link
+            color="foreground"
+            href="/"
+            className="text-2xl  text-white hover:text-orange-500 transition-all "
+          >
             Home
           </Link>
         </NavbarItem>
@@ -53,8 +58,8 @@ export default function Header() {
         <NavbarItem>
           <Link
             color="foreground"
-            href="/coin"
-            className="text-2xl text-white "
+            href="/coins"
+            className="text-2xl text-white hover:text-orange-500 transition-all "
           >
             Coins
           </Link>
@@ -62,8 +67,8 @@ export default function Header() {
         <NavbarItem>
           <Link
             color="foreground"
-            href="/coin"
-            className="text-2xl text-white "
+            href="/news"
+            className="text-2xl text-white hover:text-orange-500 transition-all "
           >
             News
           </Link>
@@ -71,8 +76,8 @@ export default function Header() {
         <NavbarItem>
           <Link
             color="foreground"
-            href="/coin"
-            className="text-2xl text-white "
+            href="/trending"
+            className="text-2xl text-white hover:text-orange-500 transition-all "
           >
             Trending
           </Link>
