53b7fce546ebb5c108f949de9f3c2f5bb5b6de4a
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-12 10:07:36 +0530

    initial commit

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -10,15 +10,14 @@ function Coin() {
   const [currency, setCurrency] = useState("usd");
   const currencySymbol = currency === "inr" ? "₹" : "$";
   const [coins, setCoins] = useState([]);
+
   useEffect(() => {
     const fetchExchangesData = async () => {
       try {
         const response = await axios.get(
           `${BaseUrl}/coins/markets?vs_currency=${currency}`
         );
-
         setCoins(response.data);
-
         setLoading(false);
       } catch (error) {
         console.error("Error fetching exchanges:", error);
@@ -28,51 +27,36 @@ function Coin() {
 
     fetchExchangesData();
   }, [currency]);
-  return (
-    <div>
-      {" "}
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="">
-          <Header />
-        </div>
-      )}
-      <>
-        <div className="  bg-[#10b97e]/50 fixed h-full w-screen border-1 overflow-y-auto   ">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="fixed right-60 ">
-              {" "}
-              <button
-                onClick={() => setCurrency("inr")}
-                className="lg:bg-white/50 bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
-              >
-                INR
-              </button>
-              <button
-                className="lg:bg-white/50 bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
-                onClick={() => setCurrency("usd")}
-              >
-                USD
-              </button>
-            </div>
-          )}
 
-          {coins.map((item, index) => (
-            <CoinCard
-              key={index}
-              id={item.id}
-              image={item.image}
-              currencySymbol={currencySymbol}
-              name={item.name.slice(0, 11)}
-              price={item.current_price.toFixed(0)}
-              cap={item.market_cap_change_percentage_24h.toFixed(2)}
-            />
-          ))}
-        </div>
-      </>
+  return (
+    <div className="bg-[#181a20] fixed h-full w-screen overflow-y-auto">
+      {loading ? <Loader /> : <Header />}
+      <div className="fixed right-60">
+        <button
+          onClick={() => setCurrency("inr")}
+          className="lg:bg-white bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
+        >
+          INR
+        </button>
+        <button
+          className="lg:bg-white bg-blue-700 m-1 text-white lg:text-black rounded-lg right-0 px-4 py-2"
+          onClick={() => setCurrency("usd")}
+        >
+          USD
+        </button>
+      </div>
+
+      {coins.map((item, index) => (
+        <CoinCard
+          key={index}
+          id={item.id}
+          image={item.image}
+          currencySymbol={currencySymbol}
+          name={item.name.slice(0, 11)}
+          price={item.current_price.toFixed(0)}
+          cap={item.market_cap_change_percentage_24h.toFixed(2)}
+        />
+      ))}
     </div>
   );
 }
